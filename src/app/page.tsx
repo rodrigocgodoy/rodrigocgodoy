baaf2cbbe31f13db1986aae6dadc259f02c2b6f9
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-12 17:10:39 -0300

    feat: Add avatar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
+      <Avatar>
+        <AvatarImage src="/logo.jpg" />
+        <AvatarFallback>RG</AvatarFallback>
+      </Avatar>
       <h1 className="text-4xl font-mono font-extrabold">Rodrigo Godoy</h1>
     </main>
   )
