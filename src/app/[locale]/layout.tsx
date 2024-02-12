690729974b253f202529979978133f7765ea0678
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-12 18:33:23 -0300

    chore: modify intl files and add new font geist

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,18 @@
-import '../../styles/globals.css'
-
+import { NextIntlClientProvider, useMessages } from 'next-intl'
 // eslint-disable-next-line camelcase
 import { unstable_setRequestLocale } from 'next-intl/server'
-import { Inter } from 'next/font/google'
+import { GeistSans } from 'geist/font/sans'
 import type { Metadata } from 'next'
 
 import { ThemeProvider } from '@/components/theme-provider'
 import { CommandDialog } from '@/components/command-dialog'
-
-const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
   title: 'Rodrigo Godoy',
   description: 'Site Rodrigo Godoy',
 }
 
-export default function RootLayout({
+export default function LocaleLayout({
   children,
   params: { locale },
 }: Readonly<{
@@ -24,19 +21,27 @@ export default function RootLayout({
 }>) {
   unstable_setRequestLocale(locale)
 
+  const messages = useMessages()
+
   return (
-    <html lang={locale} className={inter.className} suppressHydrationWarning>
+    <html
+      lang={locale}
+      className={GeistSans.className}
+      suppressHydrationWarning
+    >
       <head />
       <body className="bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <CommandDialog />
-          {children}
-        </ThemeProvider>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <CommandDialog />
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   )
