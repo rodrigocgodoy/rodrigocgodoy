41d94563509a3ce9e9d502287a194898a5971c5a
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-12 19:17:26 -0300

    ci: configure ci/cd GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/',
+  output: 'export',
+  reactStrictMode: true,
+}
 
 export default withNextIntl(nextConfig)
