a89da7de0f5982b49b77df0dd03fc70bdb24c05c
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-12 23:39:21 -0300

    feat: update class color footer and add new translation text footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,17 @@
+import { useFormatter, useTranslations } from 'next-intl'
+
 export function Footer() {
+  const format = useFormatter()
+  const t = useTranslations('footer')
+
   return (
-    <footer className="flex m-5 mx-auto max-w-2xl p-6 bg-slate-900 dark:bg-slate-50 rounded-2xl">
+    <footer className="flex m-5 mx-auto max-w-2xl p-6 bg-slate-50 dark:bg-slate-900 rounded-2xl">
       <span className="text-xs font-medium text-slate-950 dark:text-slate-50">
-        © 2024 Rodrigo Correia Godoy. Todos os direitos reservados.
+        {t('copyright', {
+          year: format.dateTime(new Date(), {
+            year: 'numeric',
+          }),
+        })}
       </span>
     </footer>
   )
