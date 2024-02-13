deeb091b1d957d157179d62ce76baed071dd59f3
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-13 07:31:11 -0300

    feat: add two images for background with opacity

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line camelcase
 import { unstable_setRequestLocale } from 'next-intl/server'
+import Image from 'next/image'
 
 import { Stack } from '@/components/stack'
 import { About } from '@/components/about'
@@ -17,6 +18,20 @@ export default function Home({
       <Intro />
       <About />
       <Stack />
+      <Image
+        src="https://www.svgrepo.com/show/493719/react-javascript-js-framework-facebook.svg"
+        alt="ReactJS"
+        width={550}
+        height={550}
+        className="fixed -right-[195px] -bottom-[195px] h-[550px] w-[550px] opacity-15"
+      />
+      <Image
+        src="https://www.svgrepo.com/show/354119/nodejs-icon.svg"
+        alt="Node.js"
+        width={400}
+        height={400}
+        className="fixed -left-[100px] -top-[100px] h-[400px] w-[400px] opacity-15"
+      />
     </main>
   )
 }
