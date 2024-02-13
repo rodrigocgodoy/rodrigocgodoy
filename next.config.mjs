af4ef0aaa98e0749d2d08718248b66d97f99c674
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-12 21:28:00 -0300

    ci: remove next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '',
-  output: 'export',
-  reactStrictMode: true,
-}
+const nextConfig = {}
 
 export default withNextIntl(nextConfig)
